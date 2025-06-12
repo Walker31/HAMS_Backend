b94210788cc0bd112a85c4beb8ebc4d1677bca5c
Aditya <walker310407@gmail.com>
2025-06-12 14:11:31 +0530

    Done with basic structure

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
+const app = express();
+const PORT = 3000;
 
+app.use(cors());
+app.use(express.json())
+
+
+app.get('/',(req,res) => {
+    res.send({
+        "message":"Route working perfectly"
+    });
+})
+
+app.listen(3000 ,console.log(`Server is runnning at ${PORT}`))
