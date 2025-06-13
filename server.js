2e5c033ff4edc2f934070d49d3b6511f63dafa5b
Walker <walker310407@gmail.com>
2025-06-13 15:57:02 +0530

    added .env

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ import express from 'express';
 import authRoutes from './routes/authRoutes.js';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 const PORT = 3000;
+
 
 app.use(cors());
 app.use(express.json())
 
-const MONGO_URL = 'mongodb+srv://HAMS_DB:HAMS_DB@cluster-hams.4svzuyn.mongodb.net/HAMS?retryWrites=true&w=majority&appName=Cluster-HAMS';
-mongoose.connect(MONGO_URL)
+mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         console.log('MongoDB Connected');
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
