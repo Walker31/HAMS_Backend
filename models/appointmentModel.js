e3468733e1740f87406791d89b28aa770643a84d
SHRUDEEP01 <shrudeepsenthil3000@gmail.com>
2025-06-14 16:01:58 +0530

    debugs in appointment

--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
 const AppointmentSchema = new mongoose.Schema({
-    date: Date,
     time: { type: Date, default: Date.now },
     patientId: String,
     doctorId: String,
