2524ed1159196e8b554ae0bf80fd64e1459f2465
Aditya <walker310407@gmail.com>
2025-06-23 14:28:16 +0530

    added cors

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ['http://localhost:5173','https://main.d1pbi2gs86puna.amplifyapp.com'], 
   credentials: true
-},{origin:'https://main.d1pbi2gs86puna.amplifyapp.com',credentials:true}));
+}));
 app.use(express.json());
 
 // Health-check route
@@ -30,13 +30,10 @@ app.get('/', (req, res) => {
 // Mount domain routes
 app.use('/doctors', doctorRoutes);
 app.use('/patients', patientRoutes);
-app.use('/appointments', appointmentRoutes);   // base CRUD
-app.use('/appointmentsEmail', emailRoutes);   // booking & email notifications
+app.use('/appointments', appointmentRoutes);
+app.use('/appointmentsEmail', emailRoutes);
 app.use('/reviews',reviewRoutes);
 app.use('/hospitals', hospitalRoutes);
-
-// Connect to MongoDB and start server
-// server.js (excerpt)
 
 mongoose
   .connect(process.env.MONGO_URL)
